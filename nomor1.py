35bb000d95887d9a1340acf7d08a84825dd1a781
lincolnhalim <75430250+lincolnhalim@users.noreply.github.com>
2020-12-06 14:54:49 +0700

    Update nomor1.py

--- nomor1.py
+++ nomor1.py
@@ -1,19 +1,3 @@
-# HH : Hours, 2 digits, range: 00 - 99
-
-# MM : Minutes, 2 digits, range: 00 - 59
-
-# SS : Seconds, 2 digits, range: 00 - 59
-
-# Case Flow: Saat dieksekusi, program akan mencetak nilai return function.
-
-# def timeConverter(seconds):
-#       .....
- 
-# Masukkan detik : 3600
-# Konversi : 01:00:00
-
-# Masukkan detik : 3665
-# Konversi : 01:01:05
 
 def convert(sec):
    sec = sec % (24 * 3600)
@@ -36,55 +20,4 @@ try:
 
 except:
     print("input yang anda masukkan salah")
-
-
-
-
-
-# # Function Initialization
-#  def counterClockwise(...):
-#      .....
-     
-# # Use the Function
-# print(counterClockwise(List_awal))
-
-# X = [[1, 2, 3],
-# [4, 5, 6],
-# [7, 8, 9]]
-
-
-# result = [[X[j][i] for j in range(len(X))] for i in range(len(X[0]))]
-
-# for r in result:
-#    print(r)
-
-
-# Mengurai dan Merajut Kata
-
-
-# def urai(y):
-#     for i in range(len(y)):
-#         for j in range(i+1):
-#             k += y[j]
-#             k += ""
-#         k += ""
-#         return k
-# def rajut(y):
-#     a = [1]
-#     b = 1
-#     for i in range(2, len(y)):
-#         b += i
-#         a.append(b)
-#     c = a.index(len(y))+1
-#     c *= -1
-#     l = y[c: len(y)]
-#     return l 
-
-# urai("saaaa")
-
-
-# limit = "code"
-# for baris in range(1, len(limit)+1):
-#     for angka in range(baris+1):
-#         print(angka, end = '')
     
